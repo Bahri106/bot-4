5710e8e38f91092f13b207add58d55ebb3db4911
Kahfi-XD <107167852+Kahfi-XD@users.noreply.github.com>
2022-08-28 15:34:08 +0700

    Update donasi.js

--- shop/donasi.js
+++ shop/donasi.js
@@ -3,25 +3,18 @@ return `DONASI BISA VIA BANK / E-MONEY
 
 *🏧 BANK*
 
-1. BRI 
-	ㅁ 8881 0887 4350 47326
-2. BCA 
-	ㅁ 3901 0887 4350 47326
-3. BNI 
-	ㅁ 881 0887 4350 47326
-4. PERMATA
-	ㅁ 852 8887 4350 47326
+Tidak tersedia 
 	
 *💰 E-MONEY*
 
 1. GOPAY
-	ㅁ 0887 4350 47326
+	ㅁ 
 2. OVO
-	ㅁ 0887 4350 47326
+	ㅁ 
 3. DANA
-	ㅁ 0887 4350 47326
+	ㅁ 085380166282
 	
-Semuanya atas nama RI****DI
+Semuanya atas nama ka*****a
 `
 	}
 
