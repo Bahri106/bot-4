f4e6a58a33165460481d99e8800572066d4e90d7
Kahfi-XD <107167852+Kahfi-XD@users.noreply.github.com>
2022-08-28 15:39:00 +0700

    Update infobot.js

--- shop/infobot.js
+++ shop/infobot.js
@@ -27,13 +27,13 @@ return `ℒ⃝👾 *B O T  S T A T I S T I C*
 ℒ⃝👨‍💻 *C O N N E C T  W I T H  M E*
 
 ⛎ Api : api-alphabot.herokuapp.com
-♈ YouTube : youtube.com/c/zeeoneofc
-♉ Github : zeeoneofc.github.io
-♊ Instagram : instagram.com/zeeoneofc
-♋ Tiktok : tiktok.com/@zeeoneofc
-♌ Telegram : t.me/zeeoneofc
-♎ Ch Telegram : t.me/zeeone_ofc
-♏ WhatsApp : bit.ly/zeeone_ofc
+♈ YouTube : https://youtube.com/channel/UC6GPl9xMWL61NAXQb3HBrRw
+♉ Github : github.com/Kahfi-XD 
+♊ Instagram : instagram.com/kahfiofc0
+♋ Tiktok : tiktok.com/@kahfifttiktok
+♌ Telegram : 
+♎ Ch Telegram : 
+♏ WhatsApp : 085295374964
 `}
 
 exports.infobot = infobot
