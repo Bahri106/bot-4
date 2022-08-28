450a4d3119b529c1aecee2ebc78afb433d70b7c9
Kahfi-XD <107167852+Kahfi-XD@users.noreply.github.com>
2022-08-28 15:31:13 +0700

    Update allpayment.js

--- shop/allpayment.js
+++ shop/allpayment.js
@@ -1,31 +1,27 @@
 const allpayment = (prefix) => {
-return `PAYMENT BY ZEEONE OFC
+return `PAYMENT BY KAHFI OFFICIAL 
 
 *🏧 BANK*
 
-1. BRI 
-	ㅁ 8881 0887 4350 47326
-2. BCA 
-	ㅁ 3901 0887 4350 47326
-3. BNI 
-	ㅁ 881 0887 4350 47326
-4. PERMATA
-	ㅁ 852 8887 4350 47326
+Tidak tersedia 
 	
 *💰 E-MONEY*
 
 1. GOPAY
-	ㅁ 0887 4350 47326
+	ㅁ 
 2. OVO
-	ㅁ 0887 4350 47326
+	ㅁ 
 3. DANA
-	ㅁ 0887 4350 47326
+	ㅁ 085380166282
 	
-Semuanya atas nama RI****DI
+Semuanya atas nama k****aja
 
 *📲 PULSA*
 
-Tidak tersedia
+1. TELKOMSEL 
+             ㅁ 085380166282
+2. TELKOMSEL
+             ㅁ 085295374964
 	
 Sebelum melakukan pembayaran ada baiknya anda menghubungi owner terlebih dahulu!
 `
